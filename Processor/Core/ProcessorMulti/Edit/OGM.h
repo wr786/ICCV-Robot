deeafd66dfd29668f4658ef7f7205fd28f8c23b1
agl-c <55969133+agl-c@users.noreply.github.com>
2020-12-30 12:50:13 +0800

    sketch
    some general setting and design

--- Processor/Core/ProcessorMulti/Edit/OGM.h
+++ Processor/Core/ProcessorMulti/Edit/OGM.h
@@ -12,7 +12,10 @@ struct Location {
 void Bresenham(int x1, int y1, int x2, int y2, std::vector<Location>& locationVec);
 void CalcShortestDistance(const Location& startPos, const Location& endPos, std::vector<Location>& locationVec);
 void calc_map(QVector<SourceDrainMono_Sensor_EncoderIMU_Data *> inputdata_0, QVector<SensorTimer_Sensor_URG_Data *> inputdata_1, double laserUnit, ProcessorMulti_Processor_Core_Vars * vars);
-
+//记录知识，什么任务应该顺时针还是逆时针
+//需要看一下现场的房间号设计知识
+int room_num;
+bool dir_know[room_num][room_num];
 #define OGM_H
 
 #endif
--- Processor/Core/ProcessorMulti/Edit/PIDControler.cpp
+++ Processor/Core/ProcessorMulti/Edit/PIDControler.cpp
@@ -24,14 +24,58 @@ std::pair<short, short>
 calc_steer(double dis, double yaw, int laserSize, short *laserData, double laserUnit,
                  ProcessorMulti_Processor_Core_Params *params, ProcessorMulti_Processor_Core_Vars *vars) {
 
+    // 首先判断是否到达目的地，如果到达就停止
+    if(vars->is_arrive) {
+        steer=vars->baseSteer;
+        speed=0;
+        return {speed, steer};
+    }
+    // 接着判断
+
     short span = 3; // 180 / 3 == 60
     int leftSum = 0, rightSum = 0;
+    int l_tot=0, r_tot=0,my_turning;
+
+
+//先不替换0，l_tot, r_tot以后也可能有用
+// 注意捕捉第一个路口是向右还是向左，双路口特判再说
+// 统计如果右侧30度的合计小于small_sum, 认为第一个路口是向右
+    for(size_t i=300; i<=laserSize; i+=2){
+       r_tot += *std::min_element(laserData + i, laserData + i + 1);
+    }
+    if(r_tot < vars->small_sum)  my_turning=0;
+
+    for(size_t i=0; i<=60; i+=2){
+        l_tot += *std::min_element(laserData + i, laserData + i + 1);
+    }
+    if(l_tot < vars->small_sum) my_turning=1;
+
+    //如果尚未标记，就开始标记第一个路口的方向
+    if((!vars->mark_1) && r_tot<vars->small_sum){
+        vars->first_turning=0; //第一个路口是向右
+        vars->mark_1 = 1; //标记我已经发现了第一个路口了
+    }
+    else if((!vars->mark_1) && l_tot<vars->small_sum){
+        vars->first_turning=1;
+        vars->mark_1 = 1;
+    }
+    //如果左右都是路口，显然按照程序顺序，会被标记为向右
+
+    //比照我是否是沿规划的方向，is_right=0时才需要判断,知道is_right=1以后照走即可
+    if(!(vars->is_right)){
+        //我在正确的顺时针  || 我在正确地逆时针
+        if((!vars->dir) && (vars->first_turning==0))  //顺时钟
+            vars->is_right=1; 
+        if(vars->dir && vars->first_turning) //逆时针
+            vars->is_right=1;
+    }
+
+
+
 //    bool leftMono = true, rightMono = true;
 
     std::replace(laserData, laserData+laserSize, 0, vars->infDistance);
-
    // laserSize == 361
-
    // sum 60 degrees
    for(size_t i = 0; i <= 120; i+=2) {
        leftSum += *std::min_element(laserData + i, laserData + i + 1);
@@ -47,11 +91,17 @@ calc_steer(double dis, double yaw, int laserSize, short *laserData, double laser
    err = err >= 0 ? err * err : -err * err;
 //   qDebug() << err << endl;
 
-   // notice that its all turning right
+   
    int detectRange = vars->forwardDetectRange;
-   if(*std::min_element(laserData - detectRange, laserData + detectRange + 1) != 0 && sumDelta < 0) {
-       // if here is a right corner and there is a wall ahead
-       err += 5;   // then turn right!
+
+   if(*std::min_element(laserData - detectRange, laserData + detectRange + 1) != 0 && sumDelta < 0 && dir==) {
+       // if here is a right corner and there is a wall ahead  // notice that its all turning right
+       err += 5;   // then turn right! 一般以右转为优先级
+      
+   }
+   else if(*std::min_element(laserData - detectRange, laserData + detectRange + 1) != 0 && sumDelta < 0 && dir==) {
+       // if here is a left corner and there is a wall ahead
+       err -= 5;   // then turn left! 有时只有左转路口、前面也有墙，就左转
    }
 
    qDebug() << err << " ";
